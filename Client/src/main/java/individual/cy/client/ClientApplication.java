3d915525c96ea68a0fc6c96802d9b7749705b91f
mystic_cg <215104920@qq.com>
2018-01-05 09:49:13 +0800

    cloud config client

---
+++ Client/src/main/java/individual/cy/client/ClientApplication.java
@@ -0,0 +1,32 @@
+package individual.cy.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: mystic
+ * @date: 2018/1/5 9:40
+ * @since: JDK1.8.0_144
+ * @version: X
+ * Description:
+ */
+@SpringBootApplication
+@RestController
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
