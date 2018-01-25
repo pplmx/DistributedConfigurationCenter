0c79f92f25ab010b47c39dbd0856f2adbbe3f240
mystic-cg <215104920@qq.com>
2018-01-25 21:26:41 +0800

    spring cloud on message bus

--- Server/src/main/java/individual/cy/server/ServerApplication.java
+++ Server/src/main/java/individual/cy/server/ServerApplication.java
@@ -2,6 +2,7 @@ package individual.cy.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -16,7 +17,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ServerApplication {
 
 	public static void main(String[] args) {
